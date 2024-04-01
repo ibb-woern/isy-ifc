7fda1eded165dd97441f61cd8ebaeb722543b85b
Hannes Wörn <hannes@woern.de>
2024-04-01 12:58:00 +0200

    Made xlsx import work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import click
 import parser.isybau as isybau
-import parser.bbsoft_ods as bbsoft
+import parser.bbsoft_xlsx as bbsoft
 from pathlib import Path
 
 
@@ -10,8 +10,12 @@ def main(input_file: Path):
     # Check if the file is an XML file. if so call the isybau parser
     if input_file.suffix == ".xml":
         isybau.parse(input_file)
-    if input_file.suffix == ".ods":
+        return
+    if input_file.suffix == ".xlsx":
         bbsoft.parse(input_file)
+        return
+    print("Unsupported file format")
+    return
 
 
 if __name__ == "__main__":
